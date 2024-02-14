aad114a47bd73316de364a70de28e32ab2fa8fdc
arttttt <artem-bambalov@yandex.ru>
2024-02-15 01:04:43 +0200

    add ktor deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,13 @@ kotlin {
             implementation(libs.square.okio)
 
             implementation(libs.koin.core)
+
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.cio)
+            implementation(libs.ktor.client.contentNegotiation)
+            implementation(libs.ktor.client.logging)
+            implementation(libs.ktor.client.auth)
+            implementation(libs.ktor.serialization.json)
         }
 
         desktopMain.dependencies {
