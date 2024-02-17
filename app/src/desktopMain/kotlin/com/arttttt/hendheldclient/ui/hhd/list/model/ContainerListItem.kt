6f641b9fb9f48f0e0416dc5cad2d31c605831ce1
arttttt <artem-bambalov@yandex.ru>
2024-02-18 01:51:08 +0200

    remove redundant code

--- app/src/desktopMain/kotlin/com/arttttt/hendheldclient/ui/hhd/list/model/ContainerListItem.kt
+++ app/src/desktopMain/kotlin/com/arttttt/hendheldclient/ui/hhd/list/model/ContainerListItem.kt
@@ -1,11 +1,10 @@
 package com.arttttt.hendheldclient.ui.hhd.list.model
 
 import com.arttttt.hendheldclient.utils.ListItem
-import kotlin.random.Random
 
 data class ContainerListItem(
     val title: String,
     val children: List<ListItem>
 ) : ListItem {
-    override val key: String = title + Random.nextLong()
+    override val key: String = title
 }
