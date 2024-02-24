9ed8879519aa14b44d3cb9dac26729ea0faac521
arttttt <artem-bambalov@yandex.ru>
2024-02-24 18:09:28 +0200

    basic parsing v2

---
+++ app/src/desktopMain/kotlin/com/arttttt/hendheldclient/data/network/model/settings/HhdFieldApiResponse2.kt
@@ -0,0 +1,53 @@
+package com.arttttt.hendheldclient.data.network.model.settings
+
+import com.arttttt.hendheldclient.data.network.serializers.HhdSettingsSerializer2
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonElement
+
+@Serializable(with = HhdSettingsSerializer2::class)
+sealed interface HhdFieldApiResponse2 {
+
+    @Serializable
+    data class Container(
+        @SerialName("title") val title: String,
+        @SerialName("hint") val hint: String,
+        @SerialName("tags") val tags: List<String>,
+        @SerialName("children") val children: Map<String, HhdFieldApiResponse2>,
+    ) : HhdFieldApiResponse2
+
+    @Serializable
+    data class Display(
+        @SerialName("default") val default: JsonElement?,
+        @SerialName("hint") val hint: String,
+        @SerialName("tags") val tags: List<String>,
+        @SerialName("title") val title: String,
+    ) : HhdFieldApiResponse2
+
+    @Serializable
+    data class Action(
+        @SerialName("title") val title: String,
+        @SerialName("hint") val hint: String,
+        @SerialName("tags") val tags: List<String>,
+        @SerialName("default") val default: JsonElement?,
+    ) : HhdFieldApiResponse2
+
+    @Serializable
+    data class BooleanPrimitive(
+        @SerialName("title") val title: String,
+        @SerialName("hint") val hint: String,
+        @SerialName("tags") val tags: List<String>,
+        @SerialName("default") val default: Boolean,
+    ) : HhdFieldApiResponse2
+
+    @Serializable
+    data class IntPrimitive(
+        @SerialName("title") val title: String,
+        @SerialName("hint") val hint: String,
+        @SerialName("tags") val tags: List<String>,
+        @SerialName("default") val default: JsonElement,
+        @SerialName("min") val min: Int,
+        @SerialName("max") val max: Int,
+        @SerialName("step") val step: JsonElement?,
+    ) : HhdFieldApiResponse2
+}
---
+++ app/src/desktopMain/kotlin/com/arttttt/hendheldclient/data/network/serializers/HhdSettingsSerializer2.kt
@@ -0,0 +1,76 @@
+package com.arttttt.hendheldclient.data.network.serializers
+
+import com.arttttt.hendheldclient.data.network.model.settings.HhdFieldApiResponse2
+import com.arttttt.hendheldclient.utils.mapValuesNutNull
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.decodeFromJsonElement
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+
+@Suppress("NAME_SHADOWING")
+object HhdSettingsSerializer2 : KSerializer<HhdFieldApiResponse2> {
+
+    private const val INCORRECT_DECODER_MESSAGE = "decoder is not a json decoder"
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("HhdFieldApiResponse2") {
+    }
+
+    override fun deserialize(decoder: Decoder): HhdFieldApiResponse2 {
+        val decoder = decoder as? JsonDecoder ?: throw IllegalArgumentException(INCORRECT_DECODER_MESSAGE)
+
+        val result = decoder
+            .decodeJsonElement()
+            .jsonObject
+            .map { (_, jElement) ->
+                jElement.jsonObject.toField()
+            }
+            .filterNotNull()
+
+        return result.first()
+    }
+
+    override fun serialize(encoder: Encoder, value: HhdFieldApiResponse2) {}
+
+    private fun JsonObject.toField(): HhdFieldApiResponse2? {
+        val type = this["type"]?.jsonPrimitive?.content
+        return when (type) {
+            "container" -> this.toContainer()
+            "display" -> json.decodeFromJsonElement<HhdFieldApiResponse2.Display>(this)
+            "action" -> json.decodeFromJsonElement<HhdFieldApiResponse2.Action>(this)
+            "bool" -> json.decodeFromJsonElement<HhdFieldApiResponse2.BooleanPrimitive>(this)
+            "int" -> json.decodeFromJsonElement<HhdFieldApiResponse2.IntPrimitive>(this)
+            else -> null
+        }
+    }
+
+    private fun JsonObject.toContainer(): HhdFieldApiResponse2.Container {
+        val title = this["title"]!!.jsonPrimitive.content
+        val hint = this["hint"]!!.jsonPrimitive.content
+        val tags = this["tags"]!!.jsonArray.map { it.jsonPrimitive.content }
+        val children = this["children"]
+            ?.jsonObject
+            ?.mapValuesNutNull { (_, child) ->
+                child.jsonObject.toField()
+            }
+            ?: emptyMap()
+
+        return HhdFieldApiResponse2.Container(
+            title = title,
+            hint = hint,
+            tags = tags,
+            children = children,
+        )
+    }
+}
