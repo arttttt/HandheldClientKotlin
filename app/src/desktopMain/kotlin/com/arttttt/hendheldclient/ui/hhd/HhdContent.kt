79f978bee43cdf39c5062f2b4749c2ce705a423b
arttttt <artem-bambalov@yandex.ru>
2024-02-27 21:32:35 +0200

    add actions row

--- app/src/desktopMain/kotlin/com/arttttt/hendheldclient/ui/hhd/HhdContent.kt
+++ app/src/desktopMain/kotlin/com/arttttt/hendheldclient/ui/hhd/HhdContent.kt
@@ -1,18 +1,26 @@
 package com.arttttt.hendheldclient.ui.hhd
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.arttttt.hendheldclient.components.hhd.HhdComponent
@@ -24,29 +32,70 @@ import com.arttttt.hendheldclient.utils.ListItem
 fun HhdContent(component: HhdComponent) {
     val state by component.states.collectAsState()
 
-    LazyColumn(
+    Box(
         modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        items(
-            items = state.items,
-            key = ListItem::key,
-        ) { item ->
-            when (item) {
-                is ContainerListItem -> ContainerItemContent(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentHeight()
-                        .border(
-                            width = 1.dp,
-                            color = Color.Red,
-                            shape = RoundedCornerShape(8.dp)
-                        ),
-                    item = item,
-                    onValueChanged = component::onValueUpdated,
-                    onResetClicked = component::onResetClicked,
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(
+                start = 8.dp,
+                top = 8.dp,
+                end = 8.dp,
+                bottom = 72.dp
+            ),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            items(
+                items = state.items,
+                key = ListItem::key,
+            ) { item ->
+                when (item) {
+                    is ContainerListItem -> ContainerItemContent(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight()
+                            .border(
+                                width = 1.dp,
+                                color = Color.Red,
+                                shape = RoundedCornerShape(8.dp)
+                            ),
+                        item = item,
+                        onValueChanged = component::onValueUpdated,
+                        onResetClicked = component::onResetClicked,
+                    )
+                }
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = 8.dp
                 )
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 16.dp,
+                        topEnd = 16.dp,
+                    )
+                )
+                .background(Color.Red)
+                .align(Alignment.BottomCenter)
+                .padding(
+                    vertical = 8.dp
+                ),
+            horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally)
+        ) {
+            Button(
+                onClick = {}
+            ) {
+                Text("apply")
+            }
+
+            Button(
+                onClick = {}
+            ) {
+                Text("reset")
             }
         }
     }
